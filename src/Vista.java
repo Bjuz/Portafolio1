e77defb3f99fdcdffcbc6f43e4d1202ceb40279d
Bjuz <jeaustin.rdz@gmil.com>
2021-09-20 23:36:07 -0600

    Agregados

--- src/Vista.java
+++ src/Vista.java
@@ -1,6 +1,12 @@
 import javax.swing.*;
 
 public class Vista {
-    private JTextField CampoCedula;
-    private JButton button1;
+    private JTextField FieldCedula;
+    private JButton BotonBuscar;
+    private JComboBox ProvinciaBox;
+    private JButton Guardar;
+    private JButton button3;
+    private JComboBox CantonBox;
+    private JComboBox DistritoBox;
+    private JTextField FieldNombre;
 }
